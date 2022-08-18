8a79aaefb3950b2b1dd50d891a48f67b9e60b48f
WoeunJung <leena09123@naver.com>
2022-08-18 23:23:17 +0900

    feat: 회원가입 DTO 정의

---
+++ app/src/main/java/com/example/i_go/feature_note/data/remote/requestDTO/SignInDTO.kt
@@ -0,0 +1,8 @@
+package com.example.i_go.feature_note.data.remote.requestDTO
+
+data class SignInDTO (
+    val username: String =  "",
+    val password: String = "",
+    val password2: String = "",
+    val email: String = ""
+)
---
+++ app/src/main/java/com/example/i_go/feature_note/data/remote/responseDTO/SignInResponseDTO.kt
@@ -0,0 +1,7 @@
+package com.example.i_go.feature_note.data.remote.responseDTO
+
+data class SignInResponseDTO (
+    val username : String =  "",
+    val email : String =  "",
+    val password: String = ""
+)
