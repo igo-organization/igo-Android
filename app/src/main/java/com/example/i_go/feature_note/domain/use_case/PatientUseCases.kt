38c31d3e8c77945f8b40848ba995ebda818e904b
WoeunJung <leena09123@naver.com>
2022-08-20 14:49:22 +0900

    refactoring: API 연결에 따른 필요없는 파일 제거

--- app/src/main/java/com/example/i_go/feature_note/domain/use_case/PatientUseCases.kt
+++
@@ -1,8 +0,0 @@
-package com.example.i_go.feature_note.domain.use_case
-
-class PatientUseCases (
-    val getPatients: GetPatients,
-    val deletePatient: DeletePatient,
-    val addPatient: AddPatient,
-    val getPatient: GetPatient
-)
