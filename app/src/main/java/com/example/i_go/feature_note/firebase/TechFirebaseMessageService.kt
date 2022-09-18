2efd073c2401c84f59280939dc7b9173592a07e0
WoeunJung <leena09123@naver.com>
2022-09-18 12:47:04 +0900

    fix: fcm 화면  깨우기 및 토큰 확인

--- app/src/main/java/com/example/i_go/feature_note/firebase/TechFirebaseMessageService.kt
+++ app/src/main/java/com/example/i_go/feature_note/firebase/TechFirebaseMessageService.kt
@@ -1,10 +1,11 @@
 package com.example.i_go.feature_note.firebase
 
+import android.annotation.SuppressLint
+import android.os.PowerManager
 import android.util.Log
-import android.widget.Toast
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
-import com.example.i_go.R
+import androidx.navigation.compose.rememberNavController
 import com.example.i_go.feature_note.data.storage.dataStore
 import com.example.i_go.feature_note.domain.util.log
 import com.google.android.gms.tasks.OnCompleteListener
@@ -14,24 +15,25 @@ import com.google.firebase.messaging.RemoteMessage
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 
+
 class TechFirebaseMessageService: FirebaseMessagingService() {
 
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         Log.v("CloudMessage", "From ${message.from}")
+        "this is cloudsMessage ${message.data}".log()
 
         if (message.data.isNotEmpty()){
-            Log.v("CloudMessage", "Message Data ${message.data}")
+            "Message Data ${message.data}".log()
         }
-        message.data?.let{
-            Log.v("CloudMessage", "Message Notification Body ${it["body"]}")
+        message.data.let{
+            "Message Notification Body ${it["body"]}".log()
         }
 
-        if (message.notification != null){
-            Log.v("CloudMessage", "Notification ${message.notification}")
-            Log.v("CloudMessage", "Notification Title ${message.notification!!.title}")
-            Log.v("CloudMessage", "Notification Body ${message.notification!!.body}")
-
+        if (message.notification != null) {
+            "Notification ${message.notification}".log()
+            "Notification Title ${message.notification!!.title}".log()
+            "Notification Body ${message.notification!!.body}".log()
         }
         FirebaseMessaging.getInstance().token.addOnCompleteListener(OnCompleteListener { task ->
 
@@ -43,18 +45,28 @@ class TechFirebaseMessageService: FirebaseMessagingService() {
             // Get new FCM registration token
             val token = task.result
             token.log()
+
         }
         )
+        // 알림 오면 화면 깨우기
+        val pm = getSystemService(POWER_SERVICE) as PowerManager
+        @SuppressLint("InvalidWakeLockTag") val wakeLock = pm.newWakeLock(
+            PowerManager.SCREEN_DIM_WAKE_LOCK or PowerManager.ACQUIRE_CAUSES_WAKEUP,
+            "TAG"
+        )
+        wakeLock.acquire(3000)
     }
 
 
     override fun onNewToken(token: String) {
+        token.log()
         GlobalScope.launch{
             saveGCMToken(token)
         }
     }
 
     private suspend fun saveGCMToken(token: String) {
+        token.log()
         val gckTokenKey = stringPreferencesKey("gcm_token")
         baseContext.dataStore.edit { pref ->
             pref[gckTokenKey] = token
